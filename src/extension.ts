e06081f7b0479f93304b5e1fcf10365a898ffa8b
Jonny Nabors <jonnynabors@gmail.com>
2020-02-07 17:00:27 -0500

    Draw the rest of the owl and publish to the store

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.testHopper",
+    async () => {
+      const filePath = vscode.window.activeTextEditor?.document.fileName;
+      const [fileName, fileDelimitter, fileExtension] = path
+        .basename(filePath!)
+        .split(".");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-hopper" is now active!');
+      let theFileIWantToHopTo = "";
+      if (isThisATestFile(fileDelimitter)) {
+        // If you're in a test file, you should find the corresponding implementation
+        theFileIWantToHopTo = `**/${fileName}.${fileExtension}`;
+      } else {
+        // You're not in a test file, but should go to one that is either **.spec.extension or **.test.extension
+        // TODO: Don't hardcode this, either look around and see if files have *.spec or *.test or just try one of both /shrug
+        theFileIWantToHopTo = `**/${fileName}.test.${fileDelimitter}`;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+      try {
+        const files = await vscode.workspace.findFiles(
+          theFileIWantToHopTo,
+          undefined,
+          undefined,
+          undefined
+        );
+        vscode.commands.executeCommand(
+          "vscode.open",
+          vscode.Uri.file(files[0].path)
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
+const isThisATestFile = (fileType: string) => {
+  return fileType === "spec" || fileType === "test";
+};
 // this method is called when your extension is deactivated
 export function deactivate() {}
