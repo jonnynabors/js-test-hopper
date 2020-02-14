1d65bbe49e4066dd9c538a96f23b45313059111b
Jonny Nabors <jonnynabors@gmail.com>
2020-02-14 14:28:54 -0500

    Use new glob for test files

--- src/extension.ts
+++ src/extension.ts
@@ -12,25 +12,26 @@ export function activate(context: vscode.ExtensionContext) {
 
       let theFileIWantToHopTo = "";
       if (isThisATestFile(fileDelimitter)) {
-        // If you're in a test file, you should find the corresponding implementation
+        // You're in a test file, you should find the corresponding implementation
         theFileIWantToHopTo = `**/${fileName}.${fileExtension}`;
       } else {
         // You're not in a test file, but should go to one that is either **.spec.extension or **.test.extension
         // TODO: Don't hardcode this, either look around and see if files have *.spec or *.test or just try one of both /shrug
-        theFileIWantToHopTo = `**/${fileName}.test.${fileDelimitter}`;
+        theFileIWantToHopTo = testFileGlob();
       }
 
       try {
-        const files = await vscode.workspace.findFiles(
-          theFileIWantToHopTo,
-          undefined,
-          undefined,
-          undefined
-        );
-        vscode.commands.executeCommand(
-          "vscode.open",
-          vscode.Uri.file(files[0].path)
-        );
+        const files = await vscode.workspace.findFiles(theFileIWantToHopTo);
+        const filePaths = files.map(file => file.path);
+        const filesWeFound = isThereATestFileWithThisName(fileName, filePaths);
+        if (filesWeFound) {
+          vscode.commands.executeCommand(
+            "vscode.open",
+            vscode.Uri.file(filesWeFound)
+          );
+        } else {
+          return;
+        }
       } catch (error) {
         console.error(error);
       }
@@ -42,5 +43,18 @@ export function activate(context: vscode.ExtensionContext) {
 const isThisATestFile = (fileType: string) => {
   return fileType === "spec" || fileType === "test";
 };
-// this method is called when your extension is deactivated
+
+// TODO: Improve this matcher. I don't know why the ones here don't work:
+// https://jestjs.io/docs/en/configuration#testmatch-arraystring
+// Using a glob tester seemed to work, this my be a nuance with VSCode's API
+const testFileGlob = () => {
+  return `**/*.{test,spec}.{ts,js,jsx}`;
+};
+
+const isThereATestFileWithThisName = (
+  fileName: string,
+  filesWeFound: string[]
+) => {
+  return filesWeFound.find(theFile => theFile.includes(fileName));
+};
 export function deactivate() {}
